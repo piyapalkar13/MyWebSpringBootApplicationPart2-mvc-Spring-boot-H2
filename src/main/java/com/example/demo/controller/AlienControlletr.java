64ef76804886df937637e78b49497906adb239e5
Admin <Admin@Anup>
2019-12-23 15:47:41 +0530

    here we have done update and delete Operation.

--- src/main/java/com/example/demo/controller/AlienControlletr.java
+++ src/main/java/com/example/demo/controller/AlienControlletr.java
@@ -4,15 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dao.AlienRepo;
 import com.example.demo.model.Alien;
-
 
 @RestController
 public class AlienControlletr {
@@ -21,14 +23,14 @@ public class AlienControlletr {
 	@Autowired
 	AlienRepo repo;
 	
-	//To get home.jsp page
+	//1.To get home.jsp page
 	@RequestMapping("/")
 	public String home()
 	{
 		return "home.jsp";
 	}
 	
-	//To save or insert data into h2 db
+	//2.To save or insert data into h2 db
 	@RequestMapping("/addAlien")
 	@PostMapping
 	public Alien addAlien(Alien alien)
@@ -37,7 +39,44 @@ public class AlienControlletr {
 		
 		return alien;
 	}
-	/*
+	
+	
+	//3.get all aliens
+	@RequestMapping(path="/aliens")
+	@GetMapping
+	public List<Alien> getAliens()
+	{
+		return repo.findAll();
+	}
+	
+	//4.get particular record on the basis of id
+	@RequestMapping("/alien/{aid}")
+	@GetMapping
+	public Optional<Alien> getAlien(@PathVariable("aid") int aid)
+	{
+		return repo.findById(aid);
+	}
+
+	//5. delete particular record on the basis of id
+	@DeleteMapping
+	@RequestMapping("/alien/delete/{id}")
+	public String deleteAlien(@PathVariable int id)
+	{
+		Alien a=repo.getOne(id);
+		
+		repo.delete(a);
+	    return "deleted";
+	}
+	
+	//6.update record
+	@PutMapping(path="/aliens" ,consumes= {"application/json"})
+	public Alien updateAlien(@RequestBody Alien alien)
+	{
+		repo.save(alien);
+		return alien;
+	}
+	
+	/* 	 	
 	@RequestMapping("/getAlien")
 	public ModelAndView getAlien(@RequestParam int aid)
 	{
@@ -51,19 +90,4 @@ public class AlienControlletr {
 		return mv;
 		
 	}*/
-	
-	//get all aliens
-	@RequestMapping(path="/aliens")
-	@GetMapping
-	public List<Alien> getAliens()
-	{
-		return repo.findAll();
-	}
-	
-	@RequestMapping("/alien/{aid}")
-	@GetMapping
-	public Optional<Alien> getAlien(@PathVariable("aid") int aid)
-	{
-		return repo.findById(aid);
-	}
 }
