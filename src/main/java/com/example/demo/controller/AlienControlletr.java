42305cef1ce462511ed4c642d17779e0773fad7f
Admin <Admin@Anup>
2019-12-23 14:03:38 +0530

    introducing concept of Rest Controller with post method.

--- src/main/java/com/example/demo/controller/AlienControlletr.java
+++ src/main/java/com/example/demo/controller/AlienControlletr.java
@@ -1,16 +1,20 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dao.AlienRepo;
 import com.example.demo.model.Alien;
 
 
-@Controller
+@RestController
 public class AlienControlletr {
 
 	//inject or create obj of AlienRepo
@@ -26,13 +30,14 @@ public class AlienControlletr {
 	
 	//To save or insert data into h2 db
 	@RequestMapping("/addAlien")
-	public String saveAlien(Alien alien)
+	@PostMapping
+	public Alien addAlien(Alien alien)
 	{
 		repo.save(alien);
 		
-		return "home.jsp";
+		return alien;
 	}
-	
+	/*
 	@RequestMapping("/getAlien")
 	public ModelAndView getAlien(@RequestParam int aid)
 	{
@@ -42,12 +47,23 @@ public class AlienControlletr {
 		mv.setViewName("showAlien.jsp");
 		mv.addObject(alien);
 
-		System.out.println(repo.findByTech("java"));
-		System.out.println(repo.findByAidGreaterThan(102));
-		System.out.println(repo.findByTechSorted("java"));
+		
 		return mv;
 		
-		
+	}*/
+	
+	//get all aliens
+	@RequestMapping(path="/aliens")
+	@GetMapping
+	public List<Alien> getAliens()
+	{
+		return repo.findAll();
 	}
 	
+	@RequestMapping("/alien/{aid}")
+	@GetMapping
+	public Optional<Alien> getAlien(@PathVariable("aid") int aid)
+	{
+		return repo.findById(aid);
+	}
 }
--- src/main/java/com/example/demo/dao/AlienRepo.java
+++ src/main/java/com/example/demo/dao/AlienRepo.java
@@ -1,17 +1,15 @@
 package com.example.demo.dao;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.Alien;
 
-public interface AlienRepo extends CrudRepository<Alien, Integer> 
+public interface AlienRepo extends JpaRepository<Alien, Integer> 
 {
-	List<Alien> findByTech(String tech);
-	List<Alien> findByAidGreaterThan(int aid);
-	
-	@Query("from Alien where tech=?1 order by aname")
-	List<Alien> findByTechSorted(String tech);
+	/*
+	 * List<Alien> findByTech(String tech); List<Alien> findByAidGreaterThan(int
+	 * aid);
+	 * 
+	 * @Query("from Alien where tech=?1 order by aname") List<Alien>
+	 * findByTechSorted(String tech);
+	 */
 }
